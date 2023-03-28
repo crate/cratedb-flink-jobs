59b765a70ec4d3f6d41a0fc6359cf3ec3671850f
Andreas Motl <andreas.motl@crate.io>
2023-03-28 16:50:43 +0200

    Add `.env` file for configuring Flink version

--- bin/build-and-submit.sh
+++ bin/build-and-submit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Build job and submit to Flink
 
+# Import settings.
+source .env
+
 # Version of the jar file.
-VERSION=$(./gradlew printVersion | grep "ProjectVersion:" |awk '{print $2}')
-JARFILE="cratedb-flink-jobs-${VERSION}.jar"
+JAR_VERSION=$(./gradlew printVersion | grep "ProjectVersion:" | awk '{print $2}')
+JARFILE="cratedb-flink-jobs-${JAR_VERSION}.jar"
 
 # Build job
 ./gradlew build
@@ -11,7 +14,7 @@ JARFILE="cratedb-flink-jobs-${VERSION}.jar"
 # Upload and invoke job
 docker run -it \
   --network=scada-demo \
-  --volume=$(pwd)/build/libs/${JARFILE}:/${JARFILE} flink:1.16.1 \
+  --volume=$(pwd)/build/libs/${JARFILE}:/${JARFILE} flink:${FLINK_VERSION} \
     \
     flink run --jobmanager=flink-jobmanager:8081 /${JARFILE} \
       --kafka.servers kafka-broker:9092 \
